88b8a8155f87a6206ce002e0dd71715bc53462cb
shruti anand <vitikaawesome81@gmail.com>
2021-05-18 14:02:19 +0530

    code to find the longest consecutive subsequence

--- HashMap/longestSequence.java
+++ HashMap/longestSequence.java
@@ -1,9 +1,55 @@
-//code to find longest subsequence in array
+//code to find longest consecutive subsequence in array
 //using hashmap
 //
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class longestSequence {
-    public static void main
+    public static ArrayList<Integer> findSequence(int arr[]){
+        //insert element in hashmap
+        HashMap<Integer,Boolean> map = new HashMap<>();
+        ArrayList<Integer> maxlen = new ArrayList<>();
+        for(int ele : arr){
+            map.put(ele,false);
+        }
+        int start=0;
+        int index=0;
+        for(int e : map.keySet()){
+            if(!map.get(e)){
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(e);
+                map.put(e,true);
+                for(int i=1;map.containsKey(e+i);i++){
+                    list.add(e+i);
+                    map.put(e+i , true);
+                    start = e;
+
+                }
+                for(int j=1;map.containsKey(e-j);j++){
+                    list.add(e-j);
+                    map.put(e-j,true);
+                    start = e-j;
+                }
+                if(maxlen.size()<list.size()){
+                    maxlen = list;
+                    index = start;
+                }
+            }
+            
+        }
+        return maxlen;
+
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        ArrayList<Integer> res= findSequence(arr);
+        for(int i=0;i<res.size();i++){
+            System.out.println(res.get(i));
+        }
+    }
 }
